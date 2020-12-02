e2b25e332a8f3d64dc345edb0a60770b0e10a47d
BricksAndPieces <ishan.h.shetty@gmail.com>
2020-12-01 23:19:22 -0600

    Day 2 Solution

--- days/day02.py
+++ days/day02.py
@@ -1,6 +1,98 @@
 """
-Problem goes here
+--- Day 2: Password Philosophy ---
+Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we
+can't log in!" You ask if you can take a look.
+
+Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the
+Official Toboggan Corporate Policy that was in effect when they were chosen.
+
+To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted
+database) and the corporate policy when that password was set.
+
+For example, suppose you have the following list:
+
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+
+Each line gives the password policy and then the password. The password
+policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For
+example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+
+In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b,
+but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits
+of their respective policies.
+
+How many passwords are valid according to their policies?
+
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official
+Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at
+the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second
+character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these
+positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy
+enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
 """
 
-with open('../inputs/day02.txt', 'r') as file:
-    inputs = [int(s) for s in file.read().split('\n')]
+from aoc import *
+
+
+def part1(inputs):
+    valid = 0
+    for line in inputs:
+        split = line.split(' ')
+        rang = split[0]
+        letter = split[1][0]
+        password = split[2]
+
+        lower = int(rang.split('-')[0])
+        upper = int(rang.split('-')[1])
+
+        letters = 0
+        for c in password:
+            if c == letter:
+                letters += 1
+
+        if upper >= letters >= lower:
+            valid += 1
+
+    return valid
+
+
+def part2(inputs):
+    valid = 0
+    for line in inputs:
+        split = line.split(' ')
+        rang = split[0]
+        letter = split[1][0]
+        password = split[2]
+
+        lower = int(rang.split('-')[0])
+        upper = int(rang.split('-')[1])
+
+        a = password[lower - 1] == letter
+        b = password[upper - 1] == letter
+
+        if a ^ b:
+            valid += 1
+
+    return valid
+
+
+puzzle_input = puzzle_input(2).split('\n')
+print(f'Part 1: {part1(puzzle_input)}')
+print(f'Part 2: {part2(puzzle_input)}')
