bdf2987d52b10d8f33bb31b5ea1c2c186eb95a98
BricksAndPieces <ishan.h.shetty@gmail.com>
2020-12-02 23:17:14 -0600

    Added support for all years

--- aoc.py
+++ aoc.py
@@ -8,12 +8,14 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def puzzle_input(day, year='2020'):
-    file_path = f'../inputs/day{day:02d}.txt'
+    project_path = path.dirname(path.abspath(__file__))
+    file_path = f'{project_path}/{year}/inputs/day{day:02d}.txt'
     if not path.exists(file_path) or path.getsize(file_path) == 0:
         print('Downloading Input...')
         opener = urllib.request.build_opener()
         opener.addheaders.append(('Cookie', f'session={__get_cookie()}'))
         url = f'https://adventofcode.com/{year}/day/{day}/input'
+        print(url)
         page = opener.open(url)
 
         with open(file_path, 'w') as file:
@@ -23,8 +25,9 @@ def puzzle_input(day, year='2020'):
         return file.read()
 
 
-def __get_cookie(file='../cookie.txt'):
-    with open(file, 'r') as file:
+def __get_cookie(file='/cookie.txt'):
+    project_path = path.dirname(path.abspath(__file__))
+    with open(project_path + file, 'r') as file:
         return file.read()
 
 
