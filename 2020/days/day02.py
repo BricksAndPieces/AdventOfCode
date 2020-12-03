e70074845b3ea976a0f12bb023e139b80530db08
BricksAndPieces <ishan.h.shetty@gmail.com>
2020-12-02 23:20:20 -0600

    changed file structure

--- 2020/days/day02.py
+++ 2020/days/day02.py
@@ -2,7 +2,7 @@
 --- Day 2: Password Philosophy ---
 Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
 
-The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something'inputs wrong with our computers; we
 can't log in!" You ask if you can take a look.
 
 Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the
