a4876f0ac25b474e23923a1c25ad5bfb5745211e
BricksAndPieces <ishan.h.shetty@gmail.com>
2020-12-02 23:22:35 -0600

    Template files for Advent of Code 2016, 2017, 2018

---
+++ 2016/days/day01.py
@@ -0,0 +1,18 @@
+"""
+Problem goes here
+"""
+
+from aoc import *
+
+
+def part1(inputs):
+    pass
+
+
+def part2(inputs):
+    pass
+
+
+puzzle_input = ints(puzzle_input(3))
+print(f'Part 1: {part1(puzzle_input)}')
+print(f'Part 2: {part2(puzzle_input)}')
