59219070ebaecd04c42ca725e29d3af52796fd6e
BricksAndPieces <ishan.h.shetty@gmail.com>
2020-12-17 19:37:26 -0600

    2020 Day 17 Solution

--- 2020/days/day17.py
+++ 2020/days/day17.py
@@ -1,6 +1,481 @@
 """
-Problem goes here
+--- Day 17: Conway Cubes ---
+As your flight slowly drifts through the sky, the Elves at the Mythical Information Bureau at the North Pole contact
+you. They'd like some help debugging a malfunctioning experimental energy source aboard one of their super-secret
+imaging satellites.
+
+The experimental energy source is based on cutting-edge technology: a set of Conway Cubes contained in a pocket
+dimension! When you hear it's having problems, you can't help but agree to take a look.
+
+The pocket dimension contains an infinite 3-dimensional grid. At every integer 3-dimensional coordinate (x,y,z), there
+exists a single cube which is either active or inactive.
+
+In the initial state of the pocket dimension, almost all cubes start inactive. The only exception to this is a small
+flat region of cubes (your puzzle input); the cubes in this region start in the specified active (#) or inactive (.)
+state.
+
+The energy source then proceeds to boot up by executing six cycles.
+
+Each cube only ever considers its neighbors: any of the 26 other cubes where any of their coordinates differ by at most
+1. For example, given the cube at x=1,y=2,z=3, its neighbors include the cube at x=2,y=2,z=2, the cube at x=0,y=2,z=3,
+and so on.
+
+During a cycle, all cubes simultaneously change their state according to the following rules:
+
+If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube
+becomes inactive.
+If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains
+inactive.
+The engineers responsible for this experimental energy source would like you to simulate the pocket dimension and
+determine what the configuration of cubes should be at the end of the six-cycle boot process.
+
+For example, consider the following initial state:
+
+.#.
+..#
+###
+Even though the pocket dimension is 3-dimensional, this initial state represents a small 2-dimensional slice of it. (In
+particular, this initial state defines a 3x3x1 region of the 3-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following configurations, where the result of each cycle is
+shown layer-by-layer at each given z coordinate (and the frame of view follows the active cells in each cycle):
+
+Before any cycles:
+
+z=0
+.#.
+..#
+###
+
+
+After 1 cycle:
+
+z=-1
+#..
+..#
+.#.
+
+z=0
+#.#
+.##
+.#.
+
+z=1
+#..
+..#
+.#.
+
+
+After 2 cycles:
+
+z=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1
+..#..
+.#..#
+....#
+.#...
+.....
+
+z=0
+##...
+##...
+#....
+....#
+.###.
+
+z=1
+..#..
+.#..#
+....#
+.#...
+.....
+
+z=2
+.....
+.....
+..#..
+.....
+.....
+
+
+After 3 cycles:
+
+z=-2
+.......
+.......
+..##...
+..###..
+.......
+.......
+.......
+
+z=-1
+..#....
+...#...
+#......
+.....##
+.#...#.
+..#.#..
+...#...
+
+z=0
+...#...
+.......
+#......
+.......
+.....##
+.##.#..
+...#...
+
+z=1
+..#....
+...#...
+#......
+.....##
+.#...#.
+..#.#..
+...#...
+
+z=2
+.......
+.......
+..##...
+..###..
+.......
+.......
+.......
+After the full six-cycle boot process completes, 112 cubes are left in the active state.
+
+Starting with your given initial configuration, simulate six cycles. How many cubes are left in the active state after
+the sixth cycle?
+
+--- Part Two ---
+For some reason, your simulated results don't match what the experimental energy source engineers expected. Apparently,
+the pocket dimension actually has four spatial dimensions, not three.
+
+The pocket dimension contains an infinite 4-dimensional grid. At every integer 4-dimensional coordinate (x,y,z,w), there
+exists a single cube (really, a hypercube) which is still either active or inactive.
+
+Each cube only ever considers its neighbors: any of the 80 other cubes where any of their coordinates differ by at most
+1. For example, given the cube at x=1,y=2,z=3,w=4, its neighbors include the cube at x=2,y=2,z=3,w=3, the cube at x=0,
+y=2,z=3,w=4, and so on.
+
+The initial state of the pocket dimension still consists of a small flat region of cubes. Furthermore, the same rules
+for cycle updating still apply: during each cycle, consider the number of active neighbors of each cube.
+
+For example, consider the same initial state as in the example above. Even though the pocket dimension is 4-dimensional,
+this initial state represents a small 2-dimensional slice of it. (In particular, this initial state defines a 3x3x1x1
+region of the 4-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following configurations, where the result of each cycle is
+shown layer-by-layer at each given z and w coordinate:
+
+Before any cycles:
+
+z=0, w=0
+.#.
+..#
+###
+
+
+After 1 cycle:
+
+z=-1, w=-1
+#..
+..#
+.#.
+
+z=0, w=-1
+#..
+..#
+.#.
+
+z=1, w=-1
+#..
+..#
+.#.
+
+z=-1, w=0
+#..
+..#
+.#.
+
+z=0, w=0
+#.#
+.##
+.#.
+
+z=1, w=0
+#..
+..#
+.#.
+
+z=-1, w=1
+#..
+..#
+.#.
+
+z=0, w=1
+#..
+..#
+.#.
+
+z=1, w=1
+#..
+..#
+.#.
+
+
+After 2 cycles:
+
+z=-2, w=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1, w=-2
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=-2
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=1, w=-2
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=-2
+.....
+.....
+..#..
+.....
+.....
+
+z=-2, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=-1, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=-1
+.....
+.....
+.....
+.....
+.....
+
+z=-2, w=0
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=-1, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=0
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=0
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=-2, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=-1, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=1, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=1
+.....
+.....
+.....
+.....
+.....
+
+z=-2, w=2
+.....
+.....
+..#..
+.....
+.....
+
+z=-1, w=2
+.....
+.....
+.....
+.....
+.....
+
+z=0, w=2
+###..
+##.##
+#...#
+.#..#
+.###.
+
+z=1, w=2
+.....
+.....
+.....
+.....
+.....
+
+z=2, w=2
+.....
+.....
+..#..
+.....
+.....
+After the full six-cycle boot process completes, 848 cubes are left in the active state.
+
+Starting with your given initial configuration, simulate six cycles in a 4-dimensional space. How many cubes are left in
+the active state after the sixth cycle?
 """
 
-with open('../inputs/day17.txt', 'r') as file:
-    inputs = [int(s) for s in file.read().split('\n')]
+from itertools import product
+
+from collections import defaultdict
+from aoc import *
+
+inputs = puzzle_input(17, 2020, sample=False).split('\n')
+
+
+def bounding_box(cells, part1):
+    min_x = min([c[0] for c in cells.keys()])
+    max_x = max([c[0] for c in cells.keys()])
+    min_y = min([c[1] for c in cells.keys()])
+    max_y = max([c[1] for c in cells.keys()])
+    min_z = min([c[2] for c in cells.keys()])
+    max_z = max([c[2] for c in cells.keys()])
+
+    if not part1:
+        min_w = min([c[3] for c in cells.keys()])
+        max_w = max([c[3] for c in cells.keys()])
+
+    coords = []
+    for x in range(min_x - 1, max_x + 2):
+        for y in range(min_y - 1, max_y + 2):
+            for z in range(min_z - 1, max_z + 2):
+                if part1:
+                    coords.append((x, y, z))
+                else:
+                    for w in range(min_w - 1, max_w + 2):
+                        coords.append((x, y, z, w))
+
+    return coords
+
+
+def solve(part1):
+    cells = defaultdict(lambda: False)
+
+    for x in range(len(inputs)):
+        for y in range(len(inputs[0])):
+            k = (x, y, 0) if part1 else (x, y, 0, 0)
+            cells[k] = inputs[x][y] == '#'
+
+    for _ in range(6):
+        new_cells = defaultdict(lambda: False)
+        for coords in bounding_box(cells, part1):
+            neighbors = 0
+            for change in product([-1, 0, 1], repeat=3 if part1 else 4):
+                if change == (0, 0, 0) or change == (0, 0, 0, 0):
+                    continue
+
+                neighbor_coords = tuple((a + b) for a, b in zip(coords, change))
+                if cells[neighbor_coords]:
+                    neighbors += 1
+
+            if cells[coords]:
+                new_cells[coords] = neighbors in [2, 3]
+            else:
+                new_cells[coords] = neighbors == 3
+
+        cells = new_cells
+
+    return sum(cells.values())
+
+
+print(f'Part 1: {solve(True)}')
+print(f'Part 2: {solve(False)}')
