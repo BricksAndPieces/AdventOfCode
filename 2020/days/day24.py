c7d30fa2a9e209064b33fb8a5817013865b8de7a
BricksAndPieces <ishan.h.shetty@gmail.com>
2020-12-24 12:00:36 -0600

    2020 Day 24 Solution

--- 2020/days/day24.py
+++ 2020/days/day24.py
@@ -1,6 +1,151 @@
 """
-Problem goes here
+--- Day 24: Lobby Layout ---
+Your raft makes it to the tropical island; it turns out that the small crab was an excellent navigator. You make your
+way to the resort.
+
+As you enter the lobby, you discover a small problem: the floor is being renovated. You can't even reach the check-in
+desk until they've finished installing the new tile floor.
+
+The tiles are all hexagonal; they need to be arranged in a hex grid with a very specific color pattern. Not in the mood
+to wait, you offer to help figure out the pattern.
+
+The tiles are all white on one side and black on the other. They start with the white side facing up. The lobby is large
+enough to fit whatever pattern might need to appear there.
+
+A member of the renovation crew gives you a list of the tiles that need to be flipped over (your puzzle input). Each
+line in the list identifies a single tile that needs to be flipped by giving a series of steps starting from a reference
+tile in the very center of the room. (Every line starts from the same reference tile.)
+
+Because the tiles are hexagonal, every tile has six neighbors: east, southeast, southwest, west, northwest, and
+northeast. These directions are given in your list, respectively, as e, se, sw, w, nw, and ne. A tile is identified by a
+series of these directions with no delimiters; for example, esenee identifies the tile you land on if you start at the
+reference tile and then move one tile east, one tile southeast, one tile northeast, and one tile east.
+
+Each time a tile is identified, it flips from white to black or from black to white. Tiles might be flipped more than
+once. For example, a line like esew flips a tile immediately adjacent to the reference tile, and a line like nwwswee
+flips the reference tile itself.
+
+Here is a larger example:
+
+sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew
+In the above example, 10 tiles are flipped once (to black), and 5 more are flipped twice (to black, then back to white).
+After all of these instructions have been followed, a total of 10 tiles are black.
+
+Go through the renovation crew's list and determine which tiles they need to flip. After all of the instructions have
+been followed, how many tiles are left with the black side up?
+
+--- Part Two ---
+The tile floor in the lobby is meant to be a living art exhibit. Every day, the tiles are all flipped according to the
+following rules:
+
+Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+Here, tiles immediately adjacent means the six tiles directly touching the tile in question.
+
+The rules are applied simultaneously to every tile; put another way, it is first determined which tiles need to be
+flipped, then they are all flipped at the same time.
+
+In the above example, the number of black tiles that are facing up after the given number of days has passed is as
+follows:
+
+Day 1: 15
+Day 2: 12
+Day 3: 25
+Day 4: 14
+Day 5: 23
+Day 6: 28
+Day 7: 41
+Day 8: 37
+Day 9: 49
+Day 10: 37
+
+Day 20: 132
+Day 30: 259
+Day 40: 406
+Day 50: 566
+Day 60: 788
+Day 70: 1106
+Day 80: 1373
+Day 90: 1844
+Day 100: 2208
+After executing this process a total of 100 times, there would be 2208 black tiles facing up.
+
+How many tiles will be black after 100 days?
 """
 
-with open('../inputs/day24.txt', 'r') as file:
-    inputs = [int(s) for s in file.read().split('\n')]
+from itertools import permutations
+from aoc import *
+
+inputs = puzzle_input(24, 2020, sample=False).split('\n')
+
+dirs = ['nw', 'se', 'ne', 'sw', 'w', 'e']
+dirs_replace = list('ABCDEF')
+
+black = set()
+for line in inputs:
+    for i, dir in enumerate(dirs):
+        line = line.replace(dir, dirs_replace[i])
+
+    x, y, z = 0, 0, 0
+    for dir in line:
+        if dir == 'A':
+            x += 1
+            z -= 1
+        elif dir == 'B':
+            x -= 1
+            z += 1
+        elif dir == 'C':
+            y += 1
+            z -= 1
+        elif dir == 'D':
+            y -= 1
+            z += 1
+        elif dir == 'E':
+            x += 1
+            y -= 1
+        elif dir == 'F':
+            x -= 1
+            y += 1
+
+    if (x, y, z) in black:
+        black.remove((x, y, z))
+    else:
+        black.add((x, y, z))
+
+print(f'Part 1: {len(black)}')
+
+for _ in range(100):
+    new_black = set()
+    test = black.copy()
+    for x, y, z in black:
+        for dx, dy, dz in permutations([-1, 0, 1]):
+            test.add((x+dx, y+dy, z+dz))
+
+    for x, y, z in test:
+        tile = (x, y, z) in black
+        n = len([0 for dx, dy, dz in permutations([-1, 0, 1]) if (x+dx, y+dy, z+dz) in black])
+        if (tile and n in [1, 2]) or (not tile and n == 2):
+            new_black.add((x, y, z))
+
+    black = new_black
+
+print(f'Part 2: {len(black)}')
