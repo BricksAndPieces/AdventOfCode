229f41883003b82913bd42dc426eee5b1f9a1ca7
IshanShetty <ishan.h.shetty@gmail.com>
2021-12-17 14:49:36 -0600

    Day 17

---
+++ 2021/days/day16.py
@@ -0,0 +1,19 @@
+"""
+Problem here
+"""
+
+from collections import defaultdict, deque
+from itertools import combinations, permutations, product
+from functools import lru_cache, reduce
+import numpy as np
+import re
+
+from aoc import *
+
+inputs = puzzle_input(16, 2021, sample=True).split('\n')
+
+for line in inputs:
+    break  # Puzzle Code
+
+print(f'Part 1: {0}')
+print(f'Part 2: {0}')
